c78d093f160dd2b2b3728ffaffe9444525ab59a5
kamanimehul <mehulkamani222@gmail.com>
2024-10-05 17:17:59 +0530

    implement ui and test in browser
    implement ui and test in browser, Fixed escaping strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StringCalculatorService } from './services/string-calculator.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'string-calculator';
+  inputString: string = '';
+  result: string = '';
+
+  constructor(private stringCalculatorService: StringCalculatorService) { }
+
+  calculate() {
+    try {
+      const sum = this.stringCalculatorService.add(this.inputString);
+      this.result = `Result: ${sum}`;
+    } catch (error: any) {
+      this.result = error.message;
+    }
+  }
 }
--- src/app/services/string-calculator.service.ts
+++ src/app/services/string-calculator.service.ts
@@ -9,28 +9,28 @@ export class StringCalculatorService {
 
   add(input: string): number {
 
-    if (!input) return 0;
-
+    if (!input) return 0; // empty string retun 0
+    input = input.replace(/\\n/g, '\n');    // Escaping Strings
+    
     let delimiter = /,|\n/;
     if (input.startsWith('//')) {
       const parts = input.split('\n');
       delimiter = new RegExp(parts[0][2]);
       input = parts[1];
     }
-    const numbers = input.split(delimiter).map(Number);
 
-    const invalid = numbers.filter(num => isNaN(num));
+    const numbers = input.split(delimiter).map(Number); // split value and return
+    const invalid = numbers.filter(num => isNaN(num)); // check invalid string and return error 
     if (invalid.length > 0) {
       throw new Error(`String is not valid`);
     } else {
-      const negatives = numbers.filter(num => num < 0);
+      const negatives = numbers.filter(num => num < 0); // check negative value and return error 
       if (negatives.length > 0) {
         throw new Error(`Negatives not allowed: ${negatives.join(',')}`);
       }
     }
 
-
-    return numbers.reduce((sum, num) => sum + num, 0);
-
+    return numbers.reduce((sum, num) => sum + num, 0);  // some of value
+    
   }
 }
